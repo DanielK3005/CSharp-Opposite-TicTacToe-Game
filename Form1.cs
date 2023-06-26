0680ccc2dd1d94adc1974b6583101cc871f641cb
DanielK3005 <danielkaramzin@gmail.com>
2023-06-26 21:24:22 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace B23_Ex05_Daniel_208063362_Lior_207899469
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace B23_Ex05_Daniel_208063362_Lior_207899469
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
