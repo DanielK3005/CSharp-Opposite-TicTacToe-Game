33567857b3626ebdacf9ec2377f42adc8c8d3339
itsme <itsme@LIOR>
2023-07-04 20:03:50 +0300

    update

--- TicTacToeMisere.cs
+++ TicTacToeMisere.cs
@@ -20,18 +20,18 @@ namespace B23_Ex05_Daniel_208063362_Lior_207899469
         private Label player2ScoreLabel;
         private GameLogic gameLogic;
 
-        public enum eGameMode
-        {
-            HumanVsHuman,
-            HumanVsComputer
-        }
+        //public enum eGameMode
+        //{
+        //    HumanVsHuman,
+        //    HumanVsComputer
+        //}
 
-        public TicTacToeMisere(int i_rows, int i_columns, string i_player1Name, string i_player2Name)
+        public TicTacToeMisere(int i_rows, int i_columns, string i_player1Name, string i_player2Name, int i_gameMode)
         {
             InitializeComponent(i_rows, i_columns, i_player1Name, i_player2Name);
 
             gameLogic = new GameLogic();
-            gameLogic.InitGameLogic(i_rows, (GameLogic.eGameMode)eGameMode.HumanVsHuman);
+            gameLogic.InitGameLogic(i_rows, (GameLogic.eGameMode)i_gameMode);
 
         }
 
@@ -69,12 +69,12 @@ namespace B23_Ex05_Daniel_208063362_Lior_207899469
 
             // Create labels for player scores
             player1ScoreLabel = new Label();
-            player1ScoreLabel.Text = i_player1Name + ": 0"; // Initial score for player 1
+            player1ScoreLabel.Text = $"{i_player1Name}: 0"; // Initial score for player 1
             player1ScoreLabel.Location = new System.Drawing.Point(0, rows * (buttonSize + spacing));
             this.Controls.Add(player1ScoreLabel);
 
             player2ScoreLabel = new Label();
-            player2ScoreLabel.Text = i_player2Name + ": 0"; // Initial score for player 2
+            player2ScoreLabel.Text = $"{i_player2Name}: 0"; // Initial score for player 2
             player2ScoreLabel.Location = new System.Drawing.Point(columns * (buttonSize + spacing) - 100, rows * (buttonSize + spacing));
             this.Controls.Add(player2ScoreLabel);
         }
@@ -91,12 +91,30 @@ namespace B23_Ex05_Daniel_208063362_Lior_207899469
             int row = coordinates.m_Row;
             int column = coordinates.m_Col;
 
-            gameLogic.MakeMove(coordinates);
+            bool isSuccess = gameLogic.MakeMove(coordinates);
 
+            if (isSuccess)
+            {
+                // Update the score labels
+                UpdateScoreLabels();
+
+                // Check if the game is over
+                if (gameLogic.GetIsFull() || gameLogic.GetIsLose())
+                {
+                    // Perform necessary actions when the game is over
+                    // For example, display a message, reset the game, etc.
+                }
+            }
+        }
+
+        private void UpdateScoreLabels()
+        {
+            Player player1 = gameLogic.GetPlayer1();
+            Player player2 = gameLogic.GetPlayer2();
 
             // Update the score labels
-
-
+            player1ScoreLabel.Text = $"{player1.GetPlayerName()}: {player1.GetPlayerScore()}";
+            player2ScoreLabel.Text = $"{player2.GetPlayerName()}: {player2.GetPlayerScore()}";
         }
     }
 }
